5f1fb9050da99dd83b53c423cce0adc76acf1967
superkun <superkunn@users.noreply.github.com>
2018-08-02 09:17:33 +0800

    Update 2104.cpp

--- poj/2104.cpp
+++ poj/2104.cpp
@@ -1,46 +1,54 @@
+
 #include<cstdio>
+#include<iostream>
+#include<cmath>
+#include<queue>
+#include<stack>
+#include<set>
+#include<map>
 #include<algorithm>
 #include<vector>
+#include<string>
+#include<cstring>
 using namespace std;
 #define rep(i,a,b) for(int i=a;i<=b;i++)
 #define per(i,a,b) for(int i=a;i>=b;i--)
 #define pb push_back
+#define mp make_pair
 #define all(x) (x).begin(),(x).end()
+typedef long long ll;
 typedef vector<int> vi;
+typedef pair<int,int> pii;
 const int MAXN=1e5+6;
-vi v;
-int n,cnt,m,x,y,k,root[MAXN],a[MAXN];
+int n,m,cnt,x,y,k,root[MAXN],a[MAXN];
 struct node{int l,r,sum;}T[MAXN*40];
+vi v;
 int getid(int x){return lower_bound(all(v),x)-v.begin()+1;}
 void update(int l,int r,int &x,int y,int pos){
-    T[++cnt]=T[y];
-    T[cnt].sum++;
-    x=cnt;
+    x=++cnt;
+    T[x]=T[y];
+    T[x].sum++;
     if(l==r)return;
-    int mid=(l+r)/2;
+    int mid=(l+r)>>1;
     if(mid>=pos)update(l,mid,T[x].l,T[y].l,pos);
     else update(mid+1,r,T[x].r,T[y].r,pos);
 }
 int query(int l,int r,int x,int y,int k){
     if(l==r)return l;
-    int mid=(l+r)/2;
     int sum=T[T[y].l].sum-T[T[x].l].sum;
+    int mid=(l+r)>>1;
     if(sum>=k)return query(l,mid,T[x].l,T[y].l,k);
     else return query(mid+1,r,T[x].r,T[y].r,k-sum);
 }
-void work(){
+int work(){
     scanf("%d%d",&n,&m);
-    rep(i,1,n){
-        scanf("%d",&a[i]);
-        v.pb(a[i]);
-    }
-    sort(all(v));
-    v.erase(unique(all(v)),v.end());
+    v.clear();
+    rep(i,1,n)scanf("%d",&a[i]),v.pb(a[i]);
+    sort(all(v)),v.erase(unique(all(v)),v.end());
+    cnt=0;
     rep(i,1,n)update(1,n,root[i],root[i-1],getid(a[i]));
-    rep(i,1,m){
-        scanf("%d%d%d",&x,&y,&k);
-        printf("%d\n",v[query(1,n,root[x-1],root[y],k)-1]);
-    }
+    rep(i,1,m)scanf("%d%d%d",&x,&y,&k),printf("%d\n",v[query(1,n,root[x-1],root[y],k)-1]);
+    return 0;
 }
 int main(){
 #ifdef superkunn
