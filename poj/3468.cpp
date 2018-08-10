7347cd78f1c81db681ad2158bcdc9f29524e5d22
superkun <superkunn@users.noreply.github.com>
2018-08-10 17:26:33 +0800

    Create 3468.cpp

---
+++ poj/3468.cpp
@@ -0,0 +1,107 @@
+#include <algorithm>
+#include  <iostream>
+#include   <cstring>
+#include    <string>
+#include    <cstdio>
+#include    <vector>
+#include    <cstdio>
+#include    <vector>
+#include     <stack>
+#include     <queue>
+#include     <cmath>
+#include       <set>
+#include       <map>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
+#define clr(a,x) memset(a,x,sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+#define lson rt<<1
+#define rson rt<<1|1
+#define le l,m,lson
+#define ri m+1,r,rson
+#define mid m=(l+r)>>1
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+/*************head******************/
+const int MAXN=1e5+5;
+ll lazy[MAXN<<2];
+ll tree[MAXN<<2];
+void push_up(int rt){
+    tree[rt]=tree[lson]+tree[rson];
+}
+void push_down(int rt,int m){
+    ll w=lazy[rt];
+    if(w){
+        lazy[lson]+=w;
+        lazy[rson]+=w;
+        tree[lson]+=w*(m-(m>>1));
+        tree[rson]+=w*(m>>1);
+        lazy[rt]=0;
+    }
+}
+void build(int l,int r,int rt){
+    lazy[rt]=0;
+    if(l==r){
+        scanf("%lld",&tree[rt]);
+        return;
+    }
+    int mid;
+    build(le);
+    build(ri);
+    push_up(rt);
+}
+void update(int L,int R,int v,int l,int r,int rt){
+    if(L<=l&&r<=R){
+        lazy[rt]+=v;
+        tree[rt]+=1ll*v*(r-l+1);
+        return;
+    }
+    push_down(rt,r-l+1);
+    int mid;
+    if(L<=m)update(L,R,v,le);
+    if(R>m)update(L,R,v,ri);
+    push_up(rt);
+}
+ll query(int L,int R,int l,int r,int rt){
+    if(L<=l&&r<=R){
+        return tree[rt];
+    }
+    push_down(rt,r-l+1);
+    int mid;
+    ll ret=0;
+    if(L<=m)ret+=query(L,R,le);
+    if(R>m)ret+=query(L,R,ri);
+    return ret;
+}
+char op[3];
+int x,y,z;
+int work(){
+    int N,Q;
+    scanf("%d%d",&N,&Q);
+    build(1,N,1);
+    while(Q--){
+        scanf("%s%d%d",op,&x,&y);
+        if(op[0]=='Q'){
+            printf("%lld\n",query(x,y,1,N,1));
+        }else{
+            scanf("%d",&z);
+            update(x,y,z,1,N,1);
+        }
+    }
+    return 0;
+}
+int main(){
+#ifdef superkunn
+    freopen("input.txt","rt",stdin);
+#endif
+    work();
+    return 0;
+}
