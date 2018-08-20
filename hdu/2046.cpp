1ab3396b8828388f2241c9bfa36d03628d769ebf
superkun <superkunn@users.noreply.github.com>
2018-08-20 16:39:11 +0800

    Create 2046.cpp

---
+++ hdu/2046.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include  <iostream>
+#include   <cstring>
+#include    <string>
+#include    <cstdio>
+#include    <vector>
+#include     <stack>
+#include     <queue>
+#include     <cmath>
+#include       <set>
+#include       <map>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
+#define clr(a,x) memset(a,x,sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+/*************head******************/
+ll a[55];
+int work(){
+    a[1]=1;
+    a[2]=2;
+    rep(i,3,50)a[i]=a[i-1]+a[i-2];
+    int n;
+    while(~scanf("%d",&n)){
+        printf("%lld\n",a[n]);
+    }
+    return 0;
+}
+int main(){
+#ifdef superkunn
+    freopen("input.txt","rt",stdin);
+#endif
+    work();
+    return 0;
+}
