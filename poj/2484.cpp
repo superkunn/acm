104692f3ef5782c465d3a55d59d400befff3aa72
superkun <superkunn@users.noreply.github.com>
2018-08-27 19:54:33 +0800

    Create 2484.cpp

---
+++ poj/2484.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include  <iostream>
+#include   <cstring>
+#include    <string>
+#include    <cstdio>
+#include    <vector>
+#include     <stack>
+#include     <queue>
+#include     <cmath>
+#include       <set>
+#include       <map>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
+#define clr(a,x) memset(a,x,sizeof(a))
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define SZ(x) ((int)(x).size())
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+/*************head******************/
+int work(){
+    int n;
+    while(scanf("%d",&n),n){
+        if(n<=2)puts("Alice");
+        else puts("Bob");
+    }
+    return 0;
+}
+int main(){
+#ifdef superkunn
+    freopen("input.txt","rt",stdin);
+#endif
+    work();
+    return 0;
+}
