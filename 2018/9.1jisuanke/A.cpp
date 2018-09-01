eb7cbfdb513113b7e948d1380e8215ee0afb3a3d
superkun <superkunn@users.noreply.github.com>
2018-09-01 20:26:42 +0800

    Create A.cpp

---
+++ 2018/9.1jisuanke/A.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    scanf("%d",&T);
+    while(T--){
+        long long x;
+        scanf("%lld",&x);
+        printf("%lld\n",x-1);
+    }
+    return 0;
+}
