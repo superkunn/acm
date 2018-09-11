3dac04e731f2528f980d070f91ff5a56d7c06c14
superkun <superkunn@users.noreply.github.com>
2018-09-11 16:19:36 +0800

    Create 123.java

---
+++ sgu/123.java
@@ -0,0 +1,27 @@
+
+ import java.io.*;
+public class Main{
+    static int a[] = new int[45];
+    static StreamTokenizer in =  new StreamTokenizer(new BufferedReader(new InputStreamReader(System.in)));
+    static PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args)throws Exception{
+        int k=getInt();
+        if(k<=2){
+            out.println(k);
+            out.flush();
+        }else{
+            int sum=2;
+            a[1]=a[2]=1;
+            for(int i=3;i<=k;i++){
+                a[i]=a[i-1]+a[i-2];
+                sum+=a[i];
+            }
+            out.println(sum);
+            out.flush();
+        }
+    }
+    static int getInt()throws IOException{
+        in.nextToken();
+        return (int) in.nval;
+    }
+}
