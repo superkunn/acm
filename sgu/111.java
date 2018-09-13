20196c0f24f0bdfd84b070d6ece93a3509644173
superkun <superkunn@users.noreply.github.com>
2018-09-13 20:29:43 +0800

    Create 111.java

---
+++ sgu/111.java
@@ -0,0 +1,98 @@
+
+import java.io.*;
+import java.math.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args){
+        InputStream inputStream = System.in;//new FileInputStream("C:\\Users\\xxx\\Downloads\\test.in");
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(in, out);
+        out.close();
+    }
+    static class Task {
+        static boolean check(BigInteger a,BigInteger x){
+            if(a.pow(2).compareTo(x)<=0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        public void solve(InputReader in, PrintWriter out) {
+            BigInteger a=in.nextBigInteger();
+            BigInteger ans= BigInteger.ZERO;
+            BigInteger l=BigInteger.ONE;
+            BigInteger r=a;
+            while (l.compareTo(r)<=0){
+                BigInteger mid=l.add(r).divide(BigInteger.valueOf(2));
+                if(check(mid,a)){
+                    ans=mid;
+                    l=mid.add(BigInteger.ONE);
+                }else{
+                    r=mid.subtract(BigInteger.ONE);
+                }
+            }
+            out.println(ans);
+        }
+
+    }
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public char[] nextCharArray() {
+            return next().toCharArray();
+        }
+
+        public boolean hasNext() {
+            try {
+                String string = reader.readLine();
+                if (string == null) {
+                    return false;
+                }
+                tokenizer = new StringTokenizer(string);
+                return tokenizer.hasMoreTokens();
+            } catch(IOException e) {
+                return false;
+            }
+        }
+        public BigInteger nextBigInteger() {
+            return new BigInteger(next());
+        }
+
+        public BigDecimal nextBigDecinal() {
+            return new BigDecimal(next());
+        }
+    }
+}
