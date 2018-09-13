e01d2544bd983d210357d95ee079139a4b9bad7f
superkun <superkunn@users.noreply.github.com>
2018-09-13 12:30:19 +0800

    Create 107.java

---
+++ sgu/107.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.math.*;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args){
+        InputStream inputStream = System.in;//new FileInputStream("C:\\Users\\wavator\\Downloads\\test.in");
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Task solver = new Task();
+        solver.solve(in, out);
+        out.close();
+    }
+    static class Task {
+
+        public void solve(InputReader in, PrintWriter out) {
+            int n=in.nextInt();
+            if(n<=8){
+                out.print(0);
+            }else if(n==9){
+                out.print(8);
+            }else{
+                n-=10;
+                out.print(72);
+                String s="0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+                for(int i=1;i<=n/1000;i++){
+                    out.print(s);
+                }
+                n=n%1000;
+                while(n-->0){
+                    out.print(0);
+                }
+            }
+        }
+
+    }
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public char[] nextCharArray() {
+            return next().toCharArray();
+        }
+
+        public boolean hasNext() {
+            try {
+                String string = reader.readLine();
+                if (string == null) {
+                    return false;
+                }
+                tokenizer = new StringTokenizer(string);
+                return tokenizer.hasMoreTokens();
+            } catch(IOException e) {
+                return false;
+            }
+        }
+        public BigInteger nextBigInteger() {
+            return new BigInteger(next());
+        }
+
+        public BigDecimal nextBigDecinal() {
+            return new BigDecimal(next());
+        }
+    }
+}
