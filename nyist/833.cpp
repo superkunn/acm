eb4fe866fa968f0c7e54076d827a28f23fdc9e83
superkun <superkunn@users.noreply.github.com>
2018-09-27 20:48:16 +0800

    Create 833.cpp

---
+++ nyist/833.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    while(~scanf("%d",&n)){
+        if(n<=2)puts("Hrdv");
+        else puts("Yougth");
+    }
+    return 0;
+}
