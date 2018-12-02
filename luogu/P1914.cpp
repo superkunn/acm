c14ea34dd21864ca002c0e58c5e2f28d60925d6a
superkun <superkunn@users.noreply.github.com>
2018-12-02 15:19:50 +0800

    Create P1914.cpp

---
+++ luogu/P1914.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include  <iostream>
+#include   <cstring>
+#include   <iomanip>
+#include    <string>
+#include    <cstdio>
+#include    <vector>
+#include    <bitset>
+#include     <stack>
+#include     <queue>
+#include     <cmath>
+#include       <set>
+#include       <map>
+using namespace std;
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+#define per(i,a,b) for(int i=a;i>=b;i--)
+#define clr(a,x) memset(a,x,sizeof(a))
+#define pb push_back
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define mp make_pair
+#define SZ(x) ((int)(x).size())
+typedef unsigned long long ull;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+/*************head******************/
+int main(){
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    for(auto &c:s){
+        c=(c-'a'+n)%26+'a';
+    }
+    cout<<s<<endl;
+    return 0;
+}
+
